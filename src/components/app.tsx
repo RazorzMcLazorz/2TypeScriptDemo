0da846df45b54c6efce0b745df87cfd61f33d290
NathanielYoung <young.nathaniel95@gmail.com>
2018-12-07 09:58:39 -0700

    2TypeScriptDemo

---
+++ src/components/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+interface IProps extends WithStyles<typeof styles> {}
+
+class DashBoard extends React.Component<IProps>
